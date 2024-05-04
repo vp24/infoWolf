f4533f1482496b365d303ab3ad7cdc22046585aa
“vp24 <vishay8@gmail.com>
2024-05-04 10:25:50 -0500

    line

--- src/FinancialCharts.jsx
+++ src/FinancialCharts.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Bar } from 'react-chartjs-2';
 import { Chart, CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
 Chart.register(CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend);
 
